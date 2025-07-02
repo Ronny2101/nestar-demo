173bc4815780f959bbabe4f40ccbceb3d2ea435d
Ronny2101 <dilshoddaminov2121@gmail.com>
2025-07-03 08:55:05 +0900

    feat: publish main layput components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,8 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import { light } from "../scss/MaterialTheme";
 import { useState } from "react";
-import "../scss/app.scss"
+import "../scss/app.scss";
+import "../scss/pc/main.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
    //@ts-ignore
@@ -13,7 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return(
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Component {...pageProps} />
+       <Component {...pageProps} />
     </ThemeProvider>  
   );
 }
